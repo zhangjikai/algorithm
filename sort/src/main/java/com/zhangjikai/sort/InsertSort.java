e192b6488b9d33797c695c176e7f7c37ceec141e
zhangjikai <zhangjk1993@gmail.com>
2017-02-28 17:27:04 +0800

    add 希尔排序

---
+++ sort/src/main/java/com/zhangjikai/sort/InsertSort.java
@@ -0,0 +1,30 @@
+package com.zhangjikai.sort;
+
+import java.util.List;
+
+/**
+ * 插入排序
+ */
+public class InsertSort<T extends Comparable> implements Sort<T> {
+
+
+    @Override
+    public List<T> sort(List<T> randomData) {
+        T tmp, tmp2;
+        int i, j;
+        for (i = 1; i < randomData.size(); i++) {
+            tmp = randomData.get(i);
+            for (j = i - 1; j >= 0; j--) {
+                tmp2 = randomData.get(j);
+                if (tmp.compareTo(tmp2) >= 0) {
+
+                    break;
+                }
+                randomData.set(j + 1, tmp2);
+            }
+            randomData.set(j + 1, tmp);
+            // System.out.println(randomData);
+        }
+        return randomData;
+    }
+}
