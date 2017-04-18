8254ea2a404f5187b19f1c43596ad3d2de3f2afd
zhangjikai <zhangjk1993@gmail.com>
2017-04-18 22:16:17 +0800

    update

--- leetcode/src/main/java/com/zhangjikai/linkedlist/LinkedListCycle2.java
+++ leetcode/src/main/java/com/zhangjikai/linkedlist/LinkedListCycle2.java
@@ -4,4 +4,32 @@ package com.zhangjikai.linkedlist;
  * Created by Jikai Zhang on 2017/4/14.
  */
 public class LinkedListCycle2 {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode fast = head, slow = head;
+        boolean hasCycle = false;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if(hasCycle) {
+            fast = head;
+            while (fast != null) {
+                fast = fast.next;
+                slow = slow.next;
+                if(fast == slow) {
+                    return  fast;
+                }
+            }
+        }
+        return null;
+    }
 }
---
+++ leetcode/src/main/java/com/zhangjikai/linkedlist/PalindromeLinkedList.java
@@ -0,0 +1,7 @@
+package com.zhangjikai.linkedlist;
+
+/**
+ * Created by Jikai Zhang on 2017/4/18.
+ */
+public class PalindromeLinkedList {
+}
